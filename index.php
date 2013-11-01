b9d807cc445f21e4c0f788edbc1692a1cf671855
Gabriel Bila <mielunegru@gmail.com>
2013-11-01 12:28:17 +0000

    Added support for company name field

--- index.php
+++ index.php
@@ -21,7 +21,9 @@
 <form method="post" action="index.php" enctype="multipart/form-data" >
       Name  <input type="text" name="name" id="name"/></br>
       Email <input type="text" name="email" id="email"/></br>
+	  Company Name <input type="text" name="company_name" id="company_name"/><br/>
       <input type="submit" name="submit" value="Submit" />
+	  
 </form>
 <?php
     $host = "eu-cdbr-azure-west-b.cloudapp.net";
@@ -41,13 +43,15 @@
         $name = $_POST['name'];
         $email = $_POST['email'];
         $date = date("Y-m-d");
+		$company_name = $_POST['company_name'];
         // Insert data
-        $sql_insert = "INSERT INTO registration_tbl (name, email, date) 
-                   VALUES (?,?,?)";
+        $sql_insert = "INSERT INTO registration_tbl (name, email, date, company_name) 
+                   VALUES (?,?,?,?)";
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $name);
         $stmt->bindValue(2, $email);
         $stmt->bindValue(3, $date);
+		$stmt->bindValue(4, $company_name);
         $stmt->execute();
     }
     catch(Exception $e) {
@@ -68,7 +72,8 @@
         foreach($registrants as $registrant) {
             echo "<tr><td>".$registrant['name']."</td>";
             echo "<td>".$registrant['email']."</td>";
-            echo "<td>".$registrant['date']."</td></tr>";
+			echo "<td>".$registrant['date']."</td>";
+			echo "<td>".$registrant['company_name']."</td></tr>";            
         }
         echo "</table>";
     } else {
